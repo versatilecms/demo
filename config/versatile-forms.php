92410a891ab8561956fec59f731dccb14182c6dd
Fábio Assunção <fabio23gt@gmail.com>
2018-09-25 04:36:02 -0300

    Atualização da chamada dos módulos no composer.json

---
+++ config/versatile-forms.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Available Inputs
+    |--------------------------------------------------------------------------
+    |
+    | This is an array of inputs available to add to each form, automatically
+    | generated on load and populated by demand.
+    |
+    */
+    'available_inputs' => [
+        'text' => 'Text',
+        'number' => 'Number',
+        'email' => 'Email',
+        'text_area' => 'Text Area',
+        'checkbox' => 'Checkbox',
+        'select' => 'Select',
+        'radio' => 'Radio',
+    ],
+];
---
+++ config/versatile-themes.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+	'themes_folder' => base_path('Themes'),
+    'assets_path' => public_path('themes'),
+	'publish_assets' => true
+];
